5fdd71b936db81a3fae0efc931fda024700a6f33
Victor Arreola <99841849+VictorFromTheInternet@users.noreply.github.com>
2022-09-12 20:12:53 +0000

    added palindrome and permutations

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/*
+
+	This program will use string input, and then output permutations, anagrams from a data file, and test if its a palindrome
+
+*/
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -11,8 +16,9 @@ using namespace std;
 
 // global var
 vector<string> dict;
+vector<string> perm_list;
 
-// file input for hash table
+// input data into vector
 void read_file(ifstream& infile){
 
 	while(infile){
@@ -23,6 +29,73 @@ void read_file(ifstream& infile){
 	}
 }
 
+void isPalindrome(string str){
+	int left = 0;
+	int right = str.length()-1;
+
+	while(left < right){
+		// false tests, (32 char diff in ASCII cases)
+		if(str[left] != str[right]){ // same case
+			if((int)(str[left]+32) != (int)str[right]	&&	// L , r
+				 (int)str[left] 		 != (int)str[right]+32){ // l, R
+
+				cout << "String: "<<str<<" is NOT a Palindrome"<<endl;
+				return;
+			}
+		} 	
+		
+		// inc dec
+		//cout << "left: "<<left<<", Right: "<<right <<endl;
+		left++;
+		right--;
+	}
+
+	cout << "String: "<<str<<" IS a Palindrome"<<endl;
+}
+
+void permutations(string str, int l, int r){
+	
+  // Base case 
+  if (l == r) {
+		perm_list.push_back(str);
+	}
+	// Permute
+  else  { 	
+		cout << str << endl;
+    for (int i = l; i <= r; i++) { 
+    	swap(str[l], str[i]); // swap indexes
+
+      permutations(str, l+1, r); // recursion 
+																 //(All branches for top levels done here)
+  
+      swap(str[l], str[i]); //backtrack (Next Letter)
+		}	
+  } 	
+}
+
+void anagram_test(string one, string two){
+	if(one.length() != two.length())
+		cout << one<<" == "<<two << " : FALSE";
+	
+	else{
+		sort(one.begin(), one.end());
+		sort(two.begin(), two.end());
+
+		if(one == two) {
+			cout << one<<" == "<<two << " : TRUE";
+			return;
+		}
+	}
+
+	cout << one<<" == "<<two << " : FALSE";
+}
+
+string to_uppercase(string input){
+	string temp = input;
+	for(int i=0; i<input.length(); i++)
+		temp[i] = ((int)temp[i] >= 97)? (int)temp[i]-32: (int)temp[i];
+	return temp;
+}
 
 
 
@@ -36,20 +109,31 @@ int main() {
 		if(!infile){cout << "ERROR could not open infile" <<endl;}
 
 	read_file(infile);
-	
+	/*
 		for(int i=0; i<dict.size(); i++){
 			ht.addItem(dict.at(i));
 		}
 	
 		ht.printTable();
-	
+	*/
 
 	// prompt user
 	string input;
 	cout << "Hello, Enter a one word string: ";
 	cin >> input;
 
-	// tests
+	input = to_uppercase(input);
+
+	// palindrome
+	isPalindrome(input);
+
+	// permutations
+	cout << "Here are all possible Permutations of your input: "<<endl;
+	cout << "-------------------------------------------------"<<endl;
+	permutations(input, 0, input.length()-1);
+	for(int i =0; i < perm_list.size(); i++){
+		cout << perm_list.at(i) << endl;
+	}
 		
 	
 }
